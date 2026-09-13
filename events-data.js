029c78d2bb1f6bf539e3406684c6f51127abe0a2
TeaKayB <dandyhighwayman@gmail.com>
2026-09-13 12:04:57 +0100

    Update events-data.js

--- events-data.js
+++ events-data.js
@@ -215,7 +215,7 @@ const HAMIEN_EVENTS = [
     endDate: "2026-12-05T19:00",
     locationType: "online",
     venue: "",
-    description: "The BSHM's annual Christmas Meeting and AGM showcases excellent ongoing research in the history of mathematics. Tom will be presenting on his and Jason's classroom research project, including the development of the HaMiEN. The entire event is free and open to all—you do not need to be a BSHM member to join in. For more information and to register see https://bshm.ac.uk"
+    description: "The BSHM's annual Christmas Meeting and AGM showcases excellent ongoing research in the history of mathematics. Tom will be presenting on his and Jason's classroom research project, including the development of the HaMiEN. The entire event is free and open to all; you do not need to be a BSHM member to join in. For more information and to register see https://bshm.ac.uk"
   },
 
 
